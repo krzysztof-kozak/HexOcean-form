a81f8c3f6c22e773accbc3408063d29ccab28e1f
kris <hithard5@gmail.com>
2022-06-22 20:16:37 +0200

    implement slide animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'slide-from-top': 'slide-from-top 0.6s ease-in-out',
+      },
+      keyframes: {
+        'slide-from-top': {
+          '0%': { transform: 'translateY(-100px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
