d2d47da7c7db5dc9333dd753bfd266034e540ba5
kris <hithard5@gmail.com>
2022-06-24 21:28:37 +0200

    convert data types to match expected format

--- src/utility/prepareData.js
+++ src/utility/prepareData.js
@@ -12,14 +12,18 @@ export default function prepareData(formData) {
     case 'pizza':
       propertiesToInclude.push('no_of_slices');
       propertiesToInclude.push('diameter');
+      formData.no_of_slices = parseInt(formData.no_of_slices);
+      formData.diameter = parseInt(formData.diameter);
       break;
 
     case 'soup':
       propertiesToInclude.push('spiciness_scale');
+      formData.spiciness_scale = parseInt(formData.spiciness_scale);
       break;
 
     case 'sandwich':
       propertiesToInclude.push('slices_of_bread');
+      formData.slices_of_bread = parseInt(formData.slices_of_bread);
       break;
 
     default:
