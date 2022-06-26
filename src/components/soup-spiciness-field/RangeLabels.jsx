fc669eed2c64e069eb075c23f487f517db8adb8a
kris <hithard5@gmail.com>
2022-06-26 22:31:26 +0200

    fix a bug with accidental form submit
    This one was quite tricky to debug. I'm not 100% sure, but I think this
    bug was caused by the clickable spiciness labels which were button elements.
    
    Button elements seems to default to "type=submit" by default when they are
    a form's children. So explicitly setting them to "type=button" seems to
    fix this issue.

--- src/components/soup-spiciness-field/RangeLabels.jsx
+++ src/components/soup-spiciness-field/RangeLabels.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
 import { useDish, useDishDispatch } from '../../context';
 
 export default function RangeLabels({ numberOfLabels }) {
   const { spiciness_scale } = useDish();
   const dishDispatch = useDishDispatch();
 
-  let labels = [];
+  function handleDispatch(scale) {
+    dishDispatch({ type: 'soup_spiciness_changed', soupSpiciness: scale });
+  }
 
+  let labels = [];
   for (let i = 1; i <= numberOfLabels; i++) {
     // spiciness_scale is initially an int, but then gets converted to a string
     // this is why we use loose equality here
@@ -16,8 +18,9 @@ export default function RangeLabels({ numberOfLabels }) {
       // these buttons will also never get re-ordered
       // so it is ok to use i as a key here
       <button
+        type="button"
         key={i}
-        onClick={dishDispatch.bind(null, { type: 'soup_spiciness_changed', soupSpiciness: i })}
+        onClick={() => handleDispatch(i)}
         className={`${currentValue && 'text-blue-500 font-bold'} flex justify-center basis-full text-s`}
       >
         {i}
